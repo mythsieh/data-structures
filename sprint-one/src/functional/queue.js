9361448060667d2cc8f99aa0543691e0260093f4
Mike Hsieh <mythsieh@gmail.com>
2015-08-20 11:11:47 -0700

    Fixed functional pattern - stack & queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,57 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counterAdd = 0;
+  var counterRemoval = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[counterAdd] = value;
+    counterAdd++;
   };
 
   someInstance.dequeue = function(){
+    if (counterRemoval < counterAdd) {
+      var returnResult = storage[counterRemoval];
+      delete storage[counterRemoval];
+      counterRemoval++;
+      return returnResult;
+    }
   };
 
   someInstance.size = function(){
+    return counterAdd - counterRemoval;
   };
 
   return someInstance;
 };
+
+var q = Queue();
+
+/* old version */
+//q.enqueue('a'); // q => { 0: 'a' }
+//q.enqueue('b'); // q => { 0: 'a', 1: 'b' }
+//console.log(q.dequeue()); // q => { 1: 'b' }
+//q.enqueue('c'); // q => { 1: 'c' }
+//console.log(q.size());
+
+/* with two separate counters */
+//q.enqueue('a'); // q => { 0: 'a' } , counterAdd = 1, counterRemoval = 0
+//q.enqueue('b'); // q => { 0: 'a', 1: 'b' }, counterAdd = 2, counterRemoval = 0
+//console.log(q.dequeue()); // q => { 1: 'b' }, counterAdd = 2, counterRemoval = 1
+//q.enqueue('c'); // q => { 1: 'b', 2: 'c'  }, counterAdd = 3, counterRemoval = 1
+//console.log(q.size());
+
+//console.log(q.dequeue()); // => 1
+//console.log( q ); // => [2,3,4,5<=]
+
+//q.enqueue('a'), counterAdd = 1, counterRemoval = 0
+//q.dequeue(), counterAdd = 1, counterRemoval = 1
+//q.dequeue(), counterAdd = 1, counterRemoval = 2
+//q.size() // => return 0
+
+// q.enqueue('a');
+// q.dequeue();
+// q.dequeue();
+// console.log(q.size());// size should be 0, but what is it now?
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,37 @@
 var Stack = function(){
   var someInstance = {};
-
+  var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[counter] = value;
+    counter++;
   };
-
+  
   someInstance.pop = function(){
+    if( counter > 0 ) {
+      counter--;
+    }
+    var lastValue = storage[counter];
+    delete storage[counter];
+    return lastValue;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
 };
+
+//var storage = {'rex': 'tearex'};
+//delete storage;
+//console.log(storage);
+
+// var s = Stack();
+// s.push('a');
+// s.push('b');
+// s.push('c');
+// console.log(s.pop());
